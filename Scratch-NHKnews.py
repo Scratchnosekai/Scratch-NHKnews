52059489f1e0ee9c63c12a56a38d0ec8d65f270a
Scratchnosekai <Scratchnosekai@outlook.com>
2024-10-24 13:45:57 +0900

    Update Scratch-NHKnews.py

--- Scratch-NHKnews.py
+++ Scratch-NHKnews.py
@@ -3,9 +3,6 @@ import scratchattach as sa
 from bs4 import BeautifulSoup
 import datetime
 
-tokyo_tz = datetime.timezone(datetime.timedelta(hours=9))
-dt = datetime.datetime.now(tokyo_tz)
-
 url_4 = "http://www3.nhk.or.jp/rss/news/cat4.xml"
 url_6 = "http://www3.nhk.or.jp/rss/news/cat6.xml"
 r_4 = requests.get(url_4)
@@ -13,6 +10,7 @@ r_6 = requests.get(url_6)
 xml_4=r_4.text
 xml_6=r_6.text
 
+date = datetime.datetime.now()
 print(date.year) 
 print(date.month)
 print(date.day) 
