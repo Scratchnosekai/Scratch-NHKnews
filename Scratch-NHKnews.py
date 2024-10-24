5a70fb256309dbe70f68c67c1437fbfc6a34b5dc
Scratchnosekai <Scratchnosekai@outlook.com>
2024-10-24 10:20:17 +0900

    Update Scratch-NHKnews.py

--- Scratch-NHKnews.py
+++ Scratch-NHKnews.py
@@ -1,9 +1,13 @@
 import requests
 import scratchattach as sa
 
-url_politics="http://www3.nhk.or.jp/rss/news/cat4.xml"
-url_international="http://www3.nhk.or.jp/rss/news/cat6.xml"
-r=requests.get(url_politics)(url_international)
-xml=r.text
-print(xml)
+url_4 = "http://www3.nhk.or.jp/rss/news/cat4.xml"
+url_6 = "http://www3.nhk.or.jp/rss/news/cat6.xml"
+r_4 = requests.get(url_4)
+r_6 = requests.get(url_6)
+xml_4=r_4.text
+xml_6=r_6.text
+print(xml_4)
+print(xml_6)
+
 
